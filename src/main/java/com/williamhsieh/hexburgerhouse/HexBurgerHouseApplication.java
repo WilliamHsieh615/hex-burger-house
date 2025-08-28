5f55d47d9ab49cf74bf5ef720eb14ac99cc5e8b4
William Hsieh <best5946@gmail.com>
2025-08-28 09:09:14 +0800

    初始化 Spring Boot project

---
+++ src/main/java/com/williamhsieh/hexburgerhouse/HexBurgerHouseApplication.java
@@ -0,0 +1,13 @@
+package com.williamhsieh.hexburgerhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexBurgerHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexBurgerHouseApplication.class, args);
+	}
+
+}
